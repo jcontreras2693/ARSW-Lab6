6e8579985f8a16a9404c67c37c58f9f1bbe27d95
Juan Contreras <judacobe2693@gmail.com>
2024-10-10 17:55:55 -0500

    Front-End - Lógica

--- src/main/resources/static/js/app.js
+++ src/main/resources/static/js/app.js
@@ -9,21 +9,22 @@ app = (function () {
       };
 
     var getBlueprintsByAuthor = function (author){
+    $('#blueprintsTable tbody').empty()
         apimock.getBlueprintsByAuthor(author,
             function(authorsBlueprints){
                 blueprints = authorsBlueprints.map(bp => ({name: bp.name, numberOfPoints: bp.points.length}))
                 blueprints.map(
                     bp => {
-                        var markup = "<tr><td><input type='checkbox' name='record'></td><td>" + bp.name + "</td><td>" + bp.numberOfPoints + "</td></tr>";
-                        $("#blueprintsTable").append(markup);
+                        var markup = "<tr><td>" + bp.name + "</td><td>" + bp.numberOfPoints + "</td><td><button type='button'>Open</button></td></tr>" ;
+                        $("#blueprintsTable tbody").append(markup);
                     }
                 )
                 var initialValue = 0;
-                var sumWithInitial = array1.reduce(
-                  (accumulator, currentValue) => accumulator + currentValue,
+                var sumWithInitial = blueprints.reduce(
+                  (accumulator, bp) => accumulator + bp.numberOfPoints,
                   initialValue,
                 );
-                $('#userPoints').text('Total user points: ');
+                $('#userPoints').text(sumWithInitial);
                 }
             );
     };
